ceb34847400ba6be55723102b4705e70c42a9554
Nabh Patodi <nabhpatodi1005@gmail.com>
2026-02-04 17:25:10 +0530

    Refactor Tools class to integrate CustomSearch and Scrape for improved web search functionality

--- backend/tools.py
+++ backend/tools.py
@@ -1,20 +1,23 @@
-from langchain_core.documents import Document
+import asyncio
 from langchain_core.tools import tool, BaseTool
+from playwright.async_api import Browser
 
-from chains import Chains
+from custom_search import CustomSearch
+from scrape import Scrape
 from database import Database
 
 class Tools:
 
-    def __init__(self, session_id: str, database: Database, browser):
-        self.__chains = Chains(database, browser)
+    def __init__(self, session_id: str, database: Database, browser: Browser):
+        self.__search = CustomSearch()
+        self.__scrape = Scrape(browser)
         self.__database = database
         self.__session_id = session_id
 
     async def web_search_tool(self, query: str) -> str:
         """Web Search tool to access documents from the web based on the given search query"""
-        __urls = await self.__chains.web_search({query: 5})
-        documents = await self.__chains.web_scrape(__urls)
+        __urls = await self.__search.search(query, 5)
+        documents = await asyncio.gather(*[self.__scrape.scrape(url, title) for url, title in __urls.items()])
         return "\n----------------\n".join([f"Title: {doc.metadata.get('title', 'None')}\nContent:{doc.page_content}\nSource: {doc.metadata.get('source', 'None')}" for doc in documents])
     
     async def vector_search_tool(self, query: str) -> str:
