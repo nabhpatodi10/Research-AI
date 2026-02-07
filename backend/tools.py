23da825cf9726273783ea82ce3cccc9fa0000471
Nabh Patodi <nabhpatodi1005@gmail.com>
2026-02-07 15:17:50 +0530

    Refactor tools.py to enhance web search functionality, improve document scraping logic, and add error handling for search results

--- backend/tools.py
+++ backend/tools.py
@@ -1,10 +1,14 @@
 import asyncio
 from langchain_core.tools import tool, BaseTool
+from langchain_core.messages import HumanMessage
+from langchain_core.documents import Document
+from langchain_google_genai import ChatGoogleGenerativeAI
 from playwright.async_api import Browser
 
 from custom_search import CustomSearch
 from scrape import Scrape
 from database import Database
+from nodes import Nodes
 
 class Tools:
 
@@ -13,15 +17,28 @@ class Tools:
         self.__scrape = Scrape(browser)
         self.__database = database
         self.__session_id = session_id
+        self.__model = ChatGoogleGenerativeAI(model = "models/gemini-flash-lite-latest")
+        self.__nodes = Nodes()
+
+    async def __get_doc_summary(self, document: Document) -> str:
+        summary = await self.__model.ainvoke(self.__nodes.generate_rolling_summary(document.page_content))
+        return summary.text.strip()
 
     async def web_search_tool(self, query: str) -> str:
         """Web Search tool to access documents from the web based on the given search query"""
         try:
             __urls = await self.__search.search(query, 5)
+            if not __urls:
+                return "No search results found."
+
             documents = await asyncio.gather(*[self.__scrape.scrape(url, title) for url, title in __urls.items()])
             documents = [doc for doc in documents if doc is not None and doc.page_content is not None and doc.page_content.strip() != ""]
+            if not documents:
+                return "Search results were found, but no scrapeable page content was extracted."
+
             await self.__database.add_data(self.__session_id, documents)
-            return "\n----------------\n".join([f"Title: {doc.metadata.get('title', 'None')}\nContent:{doc.page_content}\nSource: {doc.metadata.get('source', 'None')}" for doc in documents])
+            summaries = await asyncio.gather(*[self.__get_doc_summary(doc) for doc in documents])
+            return "\n----------------\n".join([f"Title: {doc.metadata.get('title', 'None')}\nContent:{summaries[_]}\nSource: {doc.metadata.get('source', 'None')}" for _, doc in enumerate(documents)])
         except Exception as e:
             return f"An error occured: {str(e)}"
     
